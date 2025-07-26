d65f881b042874aa6491062facd79ce032360bb0
poppynix <es-sara.ahmed20271@alexu.edu.eg>
2025-07-26 03:10:07 +0300

    removed unnecessary functions

--- models/User.js
+++ models/User.js
@@ -2,47 +2,42 @@ const Task = require('./Task');
 
 class User {
 
-    constructor(username, password, maxTaskId, token, tasks) {
+    constructor(username, password, maxTaskId, tasks) {
         this.username = username;
         this.password = password;
         this.maxTaskId = maxTaskId;
-        this.token = token;
-        this.tasks = tasks.map(t => new Task(t.id, t.title, t.done));
+        this.tasks = tasks || [];
     }
 
-    getTasks() {
-        return this.tasks;
-    }
+    // getTasks() {
+    //     return this.tasks;
+    // }
 
-    getTaskById(id) {
-        return this.tasks.find(task => task.id === id);
-    }
+    // getTaskById(id) {
+    //     return this.tasks.find(task => task.id === id);
+    // }
 
-    addTask(task) {
-        task.id = ++this.maxTaskId;
-        this.tasks.push(task);
-    }
+    // addTask(task) {
+    //     task.id = ++this.maxTaskId;
+    //     this.tasks.push(task);
+    // }
 
-    updateTask(id, done) {
-        const task = this.getTaskById(id);
-        if (task) {
-            task.done = done;
-            return task;
-        }
-        return null;
-    }
+    // updateTask(id, done) {
+    //     const task = this.getTaskById(id);
+    //     if (task) {
+    //         task.done = done;
+    //         return task;
+    //     }
+    //     return null;
+    // }
 
-    getToken() {
-        return this.token;
-    }
-
-    deleteTask(id) {
-        const index = this.tasks.findIndex(task => task.id === id);
-        if (index !== -1) {
-            return this.tasks.splice(index, 1)[0];
-        }
-        return null;
-    }
+    // deleteTask(id) {
+    //     const index = this.tasks.findIndex(task => task.id === id);
+    //     if (index !== -1) {
+    //         return this.tasks.splice(index, 1)[0];
+    //     }
+    //     return null;
+    // }
 }
 
 module.exports = User;

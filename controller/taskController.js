00801541d9be2c7d3aa5e0f80b6b48708ebbe311
poppynix <es-sara.ahmed20271@alexu.edu.eg>
2025-07-30 21:20:12 +0300

    no changes

--- controller/taskController.js
+++ controller/taskController.js
@@ -1,8 +1,4 @@
-const fs = require('fs').promises;
 const path = require('path');
-const usersFilePath = path.join(__dirname, '..', 'data', 'users.json');
-const User = require('../models/User'); 
-const Task = require('../models/Task'); 
 const taskService = require('../services/taskService');
 
 exports.getAllTasks = async (req, res) => {
@@ -25,11 +21,13 @@ exports.getTaskById = async (req, res) => {
 
 exports.createTask = async (req, res) => {
     const { title } = req.body;
-    if (!title) {
-        return res.status(400).json({ error: 'Title is required' });
+    try{
+        const task = await taskService.addTask(req.user, title);
+        res.status(201).json({success: true, message: 'Task created successfully', task });
     }
-    const task = await taskService.addTask(req.user, title);
-    res.status(201).json({success: true, message: 'Task created successfully', task });
+    catch {
+        res.status(500).json({error: error.message});
+    }
 }
 
 exports.updateTask = async (req, res) => {
@@ -43,8 +41,8 @@ exports.updateTask = async (req, res) => {
 
 exports.deleteTask = async (req, res) => {
     try {
-        const task = await taskService.deleteTask(req.user, req.params.id);
-        res.json({ success: true, message: 'Task deleted successfully', task });
+        await taskService.deleteTask(req.user, req.params.id);
+        res.json({ success: true, message: 'Task deleted successfully', Tasks: this.getAllTasks()});
     } catch (error) {
         res.status(500).json({ error: error.message });
     }

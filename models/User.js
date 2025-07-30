862d64b80d86b3b20b7ed16603f053345e72455d
poppynix <es-sara.ahmed20271@alexu.edu.eg>
2025-07-30 21:21:08 +0300

    no changes

--- models/User.js
+++ models/User.js
@@ -8,36 +8,6 @@ class User {
         this.maxTaskId = maxTaskId;
         this.tasks = tasks || [];
     }
-
-    // getTasks() {
-    //     return this.tasks;
-    // }
-
-    // getTaskById(id) {
-    //     return this.tasks.find(task => task.id === id);
-    // }
-
-    // addTask(task) {
-    //     task.id = ++this.maxTaskId;
-    //     this.tasks.push(task);
-    // }
-
-    // updateTask(id, done) {
-    //     const task = this.getTaskById(id);
-    //     if (task) {
-    //         task.done = done;
-    //         return task;
-    //     }
-    //     return null;
-    // }
-
-    // deleteTask(id) {
-    //     const index = this.tasks.findIndex(task => task.id === id);
-    //     if (index !== -1) {
-    //         return this.tasks.splice(index, 1)[0];
-    //     }
-    //     return null;
-    // }
 }
 
 module.exports = User;

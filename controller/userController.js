3a2e3dc68dfd651e68aa933f72d701077e8f621c
poppynix <es-sara.ahmed20271@alexu.edu.eg>
2025-07-30 21:20:19 +0300

    no changes

--- controller/userController.js
+++ controller/userController.js
@@ -1,8 +1,4 @@
-const fs = require('fs').promises;
-const path = require('path');
-const usersFilePath = path.join(__dirname, '..', 'data', 'users.json');
-const User = require('../models/User'); 
-const Task = require('../models/Task'); 
+const path = require('path'); 
 const userService = require('../services/userService');
 
 exports.getAllUsers = async (req, res) => {
@@ -15,12 +11,8 @@ exports.getAllUsers = async (req, res) => {
 }
 
 exports.getUserByUsername = async (req, res) => {
-    const username = req.params.username;
     try {
-        const user = await userService.getUserByUsername(username);
-        if (!user) {
-            return res.status(404).json({ error: 'User not found' });
-        }
+        const user = await userService.getUserByUsername(req.params.username);
         res.json({ success: true, user });
     } catch (error) {
         res.status(500).json({ error: error.message });
@@ -29,10 +21,6 @@ exports.getUserByUsername = async (req, res) => {
 
 exports.createUser = async (req, res) => {
     const { username, password } = req.body;
-    if (!username || !password) {
-        return res.status(400).json({ error: 'Username and password are required' });
-    }
-
     try {
         const user = await userService.addUser(username, password);
         res.status(201).json({ success: true, user });
@@ -42,40 +30,19 @@ exports.createUser = async (req, res) => {
 }
 
 exports.updateUser = async (req, res) => {
-    const username = req.params.username;
-    const dataType = req.params.type;
-    console.log(`Updating ${dataType} for user: ${username}`);
-
+    const { dataType, newData } = req.body;
     try {
-        if(dataType === "password") {
-            const { newPassword } = req.body;
-            if (!newPassword) {
-                return res.status(400).json({ error: 'Password is required' });
-            }
-            const user = await userService.changePassword(username, newPassword);
-            return res.json({ success: true, user });
-        } 
-        else if (dataType === "username") {
-            const { newUsername } = req.body;
-            if (!newUsername) {
-                return res.status(400).json({ error: 'Username is required' });
-            }
-            const user = await userService.changeUsername(username, newUsername);
-            return res.json({ success: true, user });
-        } 
-        else {
-            return res.status(400).json({ error: 'Invalid data type' });
-        }
+        const user = await userService.updateUser(req.params.username, dataType, newData);
+        res.json({ success: true, user });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 }
 
 exports.deleteUser = async (req, res) => {
-    const username = req.params.username;
     try {
-        await userService.deleteUser(username);
-        res.json({ success: true, message: 'User deleted successfully' });
+        await userService.deleteUser(req.params.username);
+        res.json({ success: true, message: 'User deleted successfully', Users: this.getAllUsers() });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }

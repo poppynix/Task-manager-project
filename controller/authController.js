b7903adeba5b74771d6ee73b37167a4a22f01d57
poppynix <es-sara.ahmed20271@alexu.edu.eg>
2025-07-30 21:19:56 +0300

    no changes

--- controller/authController.js
+++ controller/authController.js
@@ -1,20 +1,10 @@
-const fs = require('fs').promises;
 const path = require('path');
-const usersFilePath = path.join(__dirname, '..', 'data', 'users.json');
-const User = require('../models/User'); 
-const Task = require('../models/Task'); 
 const authService = require('../services/authService');
 
 exports.register = async (req, res) => {
+    const { username, password } = req.body;
     try {
-        const { username, password } = req.body;
-
-        if (!username || !password) {
-            return res.status(400).json({ error: 'Username and password are required' });
-        }
-
         const user = await authService.registerUser(username, password);
-
         res.status(201).json({ success: true, user });
     } catch (error) {
         res.status(500).json({ error: error.message });
@@ -22,16 +12,9 @@ exports.register = async (req, res) => {
 };
 
 exports.login = async (req, res) => {
-    // console.log('Login request received');
+    const { username, password } = req.body;
     try {
-        const { username, password } = req.body;
-
-        if (!username || !password) {
-            return res.status(400).json({ error: 'Username and password are required' });
-        }
-
     const token = await authService.loginUser(username, password);
-
     res.json({ message: 'Login successful', token });
     } catch (err) {
     res.status(401).json({ error: err.message });

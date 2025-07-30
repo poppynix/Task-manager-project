5a9c3c049c79977f0fdb4015e5fc0f1f2241c509
poppynix <es-sara.ahmed20271@alexu.edu.eg>
2025-07-30 21:21:18 +0300

    added  middleware

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controller/userController');
+const adminAuth = require('../middleware/adminAuth');
 
-router.get('/', userController.getAllUsers);
-router.get('/:username', userController.getUserByUsername);
-router.post('/register', userController.createUser);
-router.put('/:username/:type', userController.updateUser);
-router.delete('/:username', userController.deleteUser);
+router.get('/', adminAuth, userController.getAllUsers);
+router.get('/:username', adminAuth, userController.getUserByUsername);
+router.post('/register', adminAuth, userController.createUser);
+router.put('/:username', adminAuth, userController.updateUser);
+router.delete('/:username', adminAuth, userController.deleteUser);
 
 module.exports = router;
